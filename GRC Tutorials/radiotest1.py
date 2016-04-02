d0e25acaa46a734426f6aa0750addba99e1f9e78
emmanuelol <emmanuelort@gmail.com>
2016-04-01 21:11:10 -0600

    test on fm reciever

--- GRC Tutorials/radiotest1.py
+++ GRC Tutorials/radiotest1.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 ##################################################
 # Gnuradio Python Flow Graph
-# Title: Top Block
-# Generated: Thu Mar 31 22:45:49 2016
+# Title: radiotest1
+# Author: Emmanuel Ortiz
+# Generated: Fri Apr  1 21:05:40 2016
 ##################################################
 
 from gnuradio import analog
@@ -11,32 +12,37 @@ from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import filter
 from gnuradio import gr
+from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
 from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
 from gnuradio.wxgui import forms
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
 import osmosdr
 import wx
 
-class top_block(grc_wxgui.top_block_gui):
+class radiotest1(grc_wxgui.top_block_gui):
 
     def __init__(self):
-        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+        grc_wxgui.top_block_gui.__init__(self, title="radiotest1")
         _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
         self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
 
         ##################################################
         # Variables
         ##################################################
+        self.cent_freq = cent_freq = 99.3e6
         self.volume = volume = 1
         self.variable_slider_1 = variable_slider_1 = 50
         self.transition = transition = 1000000
         self.samp_rate = samp_rate = 2000000
-        self.quadrature = quadrature = 500000
-        self.freq1 = freq1 = 104.1
-        self.freq = freq = 104.1
+        self.quadrature = quadrature = 480000
+        self.freq1 = freq1 = cent_freq
         self.cutoff = cutoff = 100000
+        self.channel_width = channel_width = 200000
+        self.channel_freq = channel_freq = 100500000
         self.audio_dec = audio_dec = 10
 
         ##################################################
@@ -65,6 +71,38 @@ class top_block(grc_wxgui.top_block_gui):
         	proportion=1,
         )
         self.Add(_volume_sizer)
+        self.wxgui_fftsink2_0_0 = fftsink2.fft_sink_c(
+        	self.GetWin(),
+        	baseband_freq=channel_freq,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot",
+        	peak_hold=False,
+        )
+        self.Add(self.wxgui_fftsink2_0_0.win)
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
+        	self.GetWin(),
+        	baseband_freq=cent_freq,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot",
+        	peak_hold=False,
+        )
+        self.Add(self.wxgui_fftsink2_0.win)
         _variable_slider_1_sizer = wx.BoxSizer(wx.VERTICAL)
         self._variable_slider_1_text_box = forms.text_box(
         	parent=self.GetWin(),
@@ -90,18 +128,18 @@ class top_block(grc_wxgui.top_block_gui):
         self.Add(_variable_slider_1_sizer)
         self.rtlsdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + "" )
         self.rtlsdr_source_0.set_sample_rate(samp_rate)
-        self.rtlsdr_source_0.set_center_freq(freq, 0)
+        self.rtlsdr_source_0.set_center_freq(cent_freq, 0)
         self.rtlsdr_source_0.set_freq_corr(0, 0)
         self.rtlsdr_source_0.set_dc_offset_mode(0, 0)
         self.rtlsdr_source_0.set_iq_balance_mode(0, 0)
         self.rtlsdr_source_0.set_gain_mode(0, 0)
-        self.rtlsdr_source_0.set_gain(20, 0)
+        self.rtlsdr_source_0.set_gain(0, 0)
         self.rtlsdr_source_0.set_if_gain(20, 0)
         self.rtlsdr_source_0.set_bb_gain(20, 0)
         self.rtlsdr_source_0.set_antenna("", 0)
         self.rtlsdr_source_0.set_bandwidth(0, 0)
           
-        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
                 interpolation=40,
                 decimation=500,
                 taps=None,
@@ -117,24 +155,39 @@ class top_block(grc_wxgui.top_block_gui):
         	converter=forms.float_converter(),
         )
         self.Add(self._freq1_text_box)
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((volume, ))
-        self.audio_sink_1 = audio.sink(samp_rate, "", True)
+        self.audio_sink_1 = audio.sink(48000, "", True)
         self.analog_wfm_rcv_0 = analog.wfm_rcv(
-        	quad_rate=500000,
-        	audio_decimation=1,
+        	quad_rate=quadrature,
+        	audio_decimation=10,
         )
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1000, 1, 0)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.rtlsdr_source_0, 0), (self.low_pass_filter_0, 0))
-        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))
-        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_1, 0))
+        self.connect((self.rtlsdr_source_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.rtlsdr_source_0, 0), (self.wxgui_fftsink2_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.wxgui_fftsink2_0_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.analog_wfm_rcv_0, 0))
+        self.connect((self.analog_wfm_rcv_0, 0), (self.blocks_multiply_const_vxx_0, 0))
 
 
 # QT sink close method reimplementation
+
+    def get_cent_freq(self):
+        return self.cent_freq
+
+    def set_cent_freq(self, cent_freq):
+        self.cent_freq = cent_freq
+        self.set_freq1(self.cent_freq)
+        self.rtlsdr_source_0.set_center_freq(self.cent_freq, 0)
+        self.wxgui_fftsink2_0.set_baseband_freq(self.cent_freq)
 
     def get_volume(self):
         return self.volume
@@ -165,8 +218,11 @@ class top_block(grc_wxgui.top_block_gui):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
         self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.cutoff, self.transition, firdes.WIN_HAMMING, 6.76))
+        self.wxgui_fftsink2_0_0.set_sample_rate(self.samp_rate)
+        self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
 
     def get_quadrature(self):
         return self.quadrature
@@ -181,19 +237,25 @@ class top_block(grc_wxgui.top_block_gui):
         self.freq1 = freq1
         self._freq1_text_box.set_value(self.freq1)
 
-    def get_freq(self):
-        return self.freq
-
-    def set_freq(self, freq):
-        self.freq = freq
-        self.rtlsdr_source_0.set_center_freq(self.freq, 0)
-
     def get_cutoff(self):
         return self.cutoff
 
     def set_cutoff(self, cutoff):
         self.cutoff = cutoff
         self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.cutoff, self.transition, firdes.WIN_HAMMING, 6.76))
+
+    def get_channel_width(self):
+        return self.channel_width
+
+    def set_channel_width(self, channel_width):
+        self.channel_width = channel_width
+
+    def get_channel_freq(self):
+        return self.channel_freq
+
+    def set_channel_freq(self, channel_freq):
+        self.channel_freq = channel_freq
+        self.wxgui_fftsink2_0_0.set_baseband_freq(self.channel_freq)
 
     def get_audio_dec(self):
         return self.audio_dec
@@ -212,7 +274,7 @@ if __name__ == '__main__':
             print "Warning: failed to XInitThreads()"
     parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
     (options, args) = parser.parse_args()
-    tb = top_block()
+    tb = radiotest1()
     tb.Start(True)
     tb.Wait()
 
